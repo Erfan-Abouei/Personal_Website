aced7086caa8753c89ecc65422405ea0f095e4d4
Erfan-Abouei <erfangamer138787@gmail.com>
2025-06-29 16:16:16 +0330

    delete: hero section | vite.svg

--- src/components/ui/Pattern.tsx
+++ src/components/ui/Pattern.tsx
@@ -1,7 +1,7 @@
 const Pattern = () => {
     return (
         <div className="-z-10 absolute top-0 left-0 right-0 select-none pointer-events-none">
-            <img src="/public/images/Pattern.png" alt="Pattern"  className="object-cover size-full"/>
+            <img src="/images/Pattern.png" alt="Pattern"  className="object-cover size-full"/>
         </div>
     )
 }
