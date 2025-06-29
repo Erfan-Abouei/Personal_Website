a06d51887e2a4e803a99965ccf52f27e1fbe3dc8
Erfan-Abouei <erfangamer138787@gmail.com>
2025-06-29 21:47:52 +0330

    add: animated cursor with a react package

--- src/pages/home/components/HeroSection.tsx
+++ src/pages/home/components/HeroSection.tsx
@@ -12,7 +12,7 @@ const HeroSection = (): JSX.Element => {
     })
 
     return (
-        <section className="hero-section my-25 max-md:my-12">
+        <section className="hero-section mt-25 max-md:mt-12 mb-2">
             <div className="container">
                 {/* Section Body */}
                 <div className="flex flex-col items-center justify-center">
@@ -21,7 +21,7 @@ const HeroSection = (): JSX.Element => {
                     <div className="mt-10 max-md:mt-6 flex flex-col gap-y-6 justify-center items-center max-md:gap-y-3 text-3xl max-md:text-base font-Morabba-Bold">
                         <h3 className="text-white drop-shadow-custom drop-shadow-white/50">برای طراحی سایتت به چی نیاز داری ؟</h3>
                         <h3 className="text-white/50">وردپرس ؟ قالب اماده ؟ وبسایت های طراحی سایت ؟</h3>
-                        <h3 className="text-Alpine drop-shadow-custom drop-shadow-Alpine max-md:h-6 h-9">{displayText}</h3>
+                        <h3 className="text-Gold drop-shadow-custom drop-shadow-Gold max-md:h-6 h-9">{displayText}</h3>
                         {/* Desc */}
                         <p className="max-w-138 text-sm max-md:text-custom text-white/30 font-Dana my-6 max-md:my-4 text-center">
                             چرا؟ چون امروزه  کسایی که دانش فنی ندارن میدونن یه وب‌سایت حرفه‌ای چقدر روی تصویر برند تأثیر می‌ذاره. واقعیت اینه که مهارت‌های فرانت‌اند دیگه فقط یه تخصص فنی نیست؛ شده یه ابزار اصلی برای ساختن برند شخصی یا کاری! پس اگه روی طراحی رابط کاربری، انیمیشن‌ها، تجربه کاربری و حتی بهینه‌سازی عملکرد تمرکز کنی، می‌تونی از یه برنامه‌نویس معمولی به یه توسعه‌دهنده حرفه‌ای و شناخته‌شده تبدیل بشی.
