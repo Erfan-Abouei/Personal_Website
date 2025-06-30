28246bee320a6fc0ae2c31e2143b139a16b49f06
Erfan-Abouei <erfangamer138787@gmail.com>
2025-06-30 13:58:05 +0330

    finish ( real xD )

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,7 +1,8 @@
 import { Route } from "react-router"
 import { lazy } from "react"
-import NotFoundPage from "@/pages/not-found/NotFoundPage"
+
 const HomePageLayout = lazy(() => import('@/pages/home/HomePageLayout'))
+const NotFoundPage = lazy(() => import('@/pages/not-found/NotFoundPage'))
 
 const AllRoute = [
     <Route index element={< HomePageLayout />} key="home" />,
