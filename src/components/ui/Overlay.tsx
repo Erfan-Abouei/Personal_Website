af80a416ffd83e3b547358b8f9701a7f28fa52d0
Erfan-Abouei <erfangamer138787@gmail.com>
2025-06-30 14:09:09 +0330

    finish ( real xD )

--- src/components/ui/Overlay.tsx
+++ src/components/ui/Overlay.tsx
@@ -3,7 +3,7 @@ import type { JSX } from "react"
 
 const Overlay = ({ isShow }: OverlayProp): JSX.Element => {
     return (
-        <div className={`z-20 fixed inset-0 size-full backdrop-blur-lg transition-all ${isShow ? 'visible opacity-100' : 'invisible opacity-0'}`}></div>
+        <div className={`z-20 fixed inset-0 size-full backdrop-blur-lg transition-all duration-500 ${isShow ? 'visible opacity-100' : 'invisible opacity-0'}`}></div>
     )
 }
 
